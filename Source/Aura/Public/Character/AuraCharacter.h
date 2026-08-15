b4cbd8c9c6a4c6872114171ef29e089fc6b06327
VIP666-star <981613301@qq.com>
2026-08-15 11:05:13 +0800

    Aura Character and Enemy

---
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -0,0 +1,17 @@
+// GameplayAbilitySystem Project Aura
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/AuraCharacterBase.h"
+#include "AuraCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraCharacter : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+	
+};

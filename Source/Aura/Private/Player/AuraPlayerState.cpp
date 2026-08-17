23b6b612ec5c4c2f84ec2a2baf897b2b2c33a5d2
VIP666-star <981613301@qq.com>
2026-08-18 07:06:51 +0800

    为玩家和敌人设置ReplicationMode

--- Source/Aura/Private/Player/AuraPlayerState.cpp
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -3,9 +3,17 @@
 
 #include "Player/AuraPlayerState.h"
 
+#include "AbilitySystemComponent.h"
+
 AAuraPlayerState::AAuraPlayerState()
 {
 	NetUpdateFrequency = 100.f;
+	
+	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Minimal);
+	
+	AttributeSet = CreateDefaultSubobject<UAttributeSet>("AttributeSet");
 }
 
 UAbilitySystemComponent* AAuraPlayerState::GetAbilitySystemComponent() const

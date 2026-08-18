dca7ac917b365f5848dd46fdf4ea927f98df927f
VIP666-star <981613301@qq.com>
2026-08-19 04:58:11 +0800

    AuraHUD

---
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,14 @@
+// GameplayAbilitySystem Project Aura
+
+
+#include "UI/HUD/AuraHUD.h"
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void AAuraHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UAuraUserWidget* Widget = CreateWidget<UAuraUserWidget>(GetWorld(), OverlayWidgetClass);
+	Widget->AddToViewport();
+}

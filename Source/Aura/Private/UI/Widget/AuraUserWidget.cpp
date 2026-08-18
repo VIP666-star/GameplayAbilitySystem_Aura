421ebf29c40f5efbf5c0a536bacd6686885b508c
VIP666-star <981613301@qq.com>
2026-08-19 02:47:48 +0800

    UserWidget and WidgetController

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// GameplayAbilitySystem Project Aura
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
---
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -0,0 +1,5 @@
+// GameplayAbilitySystem Project Aura
+
+
+#include "UI/WidgetController/AuraWidgetController.h"
+
